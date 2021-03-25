33c07b0a9d677ee5633db7f4e765e0354084b043
Alex-Coplin <alexander.coplin37@gmail.com>
2021-03-24 20:46:07 -0700

    Created external call to retrieve html title

--- server/index.js
+++ server/index.js
@@ -3,8 +3,30 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 const port = 1234;
+const axios = require('axios');
 
 app.use(express.json());
 app.use(cors());
+
+const getTargetHTML = (targetUrl) => {
+  return axios.get(targetUrl)
+  .then((res) => {
+    return res.data
+  })
+  .catch((error) => {
+    error
+  });
+};
+
+const testUrl = 'https://google.com';
+getTargetHTML(testUrl).then((data) => {
+  let title = data.split('title>')[1];
+  if (title) {
+    title = title.split('<')[0];
+    console.log(title);
+  } else {
+    console.log('invalid url');
+  }
+});
 
 app.listen(port, () => console.log(`server running on port ${port}.`));
